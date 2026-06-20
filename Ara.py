6e2a0a18a445387d8822d92de8262e2b20200941
araf57721 <kabiraraf12@gmail.com>
2026-06-20 18:08:10 +0600

    added count

---
+++ Ara.py
@@ -0,0 +1,41 @@
+for i in range(5):
+    for j in range(7):
+        if(i == 0):
+            if(j == 1 or j == 2 or j == 4 or j ==5):
+                print("&", end = " ")
+            elif(j == 6):
+                print(" ")
+            else:
+                print(" ", end = " ")    
+
+        elif(i == 1):
+            if(j == 0 or j == 3 ):
+                print("&", end = " ")
+            elif(j == 6):
+                print("&")
+            else:
+                print(" ", end = " ")
+
+        elif(i == 2):
+            if(j == 1 or j == 5 ):
+                print("&", end = " ")
+            elif(j == 6):
+                print(" ")
+            else:
+                print(" ", end = " ")
+
+        elif(i == 3):
+            if(j == 2 or j == 4 ):
+                print("&", end = " ")
+            elif(j == 6):
+                print(" ")
+            else:
+                print(" ", end = " ")
+
+        elif(i == 4):
+            if( j == 3 ):
+                print("&", end = " ")
+            elif(j == 6):
+                print(" ")
+            else:
+                print(" ", end = " ")
